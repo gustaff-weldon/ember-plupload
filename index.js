448bdec0462bab75f8c9a067309315b650b15ae8
Jiří Prokop <jprokop@synaptiko.cz>
2017-08-09 17:06:45 +0200

    Release 1.3.21-bz Bayzat version

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 /* jshint node: true */
 'use strict';
 
+// For ember-cli < 2.7 findHost doesnt exist so we backport from that version
+// for earlier version of ember-cli.
+// https://github.com/ember-cli/ember-cli/blame/16e4492c9ebf3348eb0f31df17215810674dbdf6/lib/models/addon.js#L533
+function findHostShim() {
+  let current = this;
+  let app;
+  do {
+    app = current.app || app;
+  } while (current.parent.parent && (current = current.parent));
+  return app;
+}
+
 module.exports = {
   name: 'ember-plupload',
 
-  included: function (app) {
-    this._super.included(app);
+  included: function (/* appOrAddon */) {
+    this._super.included.apply(this, arguments);
+
+    let findHost = this._findHost || findHostShim;
+    let app = findHost.call(this);
+
+    this.app = app;
+
     var config = this.app.project.config(app.env) || {};
     var addonConfig = config[this.name] || {};
     var debugMode = addonConfig.debug;
